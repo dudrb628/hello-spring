6fcf4354dabcf50926b0306ca24e514951da240e
dudrb628 <86199336+dudrb628@users.noreply.github.com>
2022-07-16 18:26:19 +0900

    update boardList

--- src/main/java/com/example/hellospring/controller/BoardController.java
+++ src/main/java/com/example/hellospring/controller/BoardController.java
@@ -1,13 +1,25 @@
 package com.example.hellospring.controller;
 
+import com.example.hellospring.entity.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class BoardController {
     @GetMapping(value = "/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping(value = "/api/boardList")
+    @ResponseBody
+    public List<Board> boardList(){
+        List<Board> boardList = new ArrayList<>();
+        return boardList;
     }
 }
